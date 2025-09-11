00f22fa043b357bc65fa4ef6fa7269598cbebf5a
Vishesh Jain <visheshj2005@gmail.com>
2025-09-11 14:39:29 +0530

    Day 19

---
+++ Day_19/script1.js
@@ -0,0 +1,39 @@
+// document.getElementById("signup").addEventListener("submit",(e)=>{
+//     e.preventDefault();
+//     let name = document.getElementById("name").value;
+//     let email = document.getElementById("email").value;
+//     let phone = document.getElementById("phone").value;
+//     let password = document.getElementById("password").value;
+//     console.log(`name : ${name} email : ${email} phone : ${phone} password : ${password}`);
+// })
+
+
+//Async JS
+// setTimeOut
+// setInterval
+// clearTimeOut
+// clearInterval
+
+//setTimeOut
+// setTimeout(()=>{
+//     console.log("Hello World")
+// },2000);
+
+//setInterval
+
+// let interval = setInterval(()=>{
+//     console.log("Hello World");
+// },2000);
+// setTimeout(()=>{
+//     clearInterval(interval);
+// },10031)
+
+// console.log("First");
+// console.log("Second");
+// setTimeout(()=>{
+//     console.log("Third");
+// },2000);
+// console.log("Fourth");
+// setTimeout(()=>{
+//     console.log("Fifth");
+// },1000);
